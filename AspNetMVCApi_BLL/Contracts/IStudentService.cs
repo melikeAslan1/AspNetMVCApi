d55b8d27e1dd7565efa3f75b704a6a906bc0bb06
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-05-17 14:57:45 +0300

    BLL katmanında student Service oluşturuldu. StudentController PL katmanına eklendi. update-database ile veritabanı oluşturuldu. Yarın Aspnet MVC için DI uygulanacaktır ve Api nin metotları kontrol edilecektir.

---
+++ AspNetMVCApi_BLL/Contracts/IStudentService.cs
@@ -0,0 +1,28 @@
+﻿using AspNetMVCApi_EL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetMVCApi_BLL.Contracts
+{
+    public interface IStudentService   //bu kısım diğer projedeki engine kısmıdır.
+    {
+        ResponseData GetAllStudent();
+
+        ResponseData AddStudent(StudentVM student);
+
+        ResponseData UpdateStudent(int studentId, string newName, string newSurname);
+
+        ResponseData DeleteStudent(int studentId);
+
+
+
+
+
+
+
+
+    }
+}
--- AspNetMVCApi_EL/ViewModels/ResponseData.cs
+++ AspNetMVCApi_EL/ViewModels/ResponseData.cs
@@ -8,7 +8,7 @@ namespace AspNetMVCApi_EL.ViewModels
 {
     public class ResponseData
     {
-        public int Message { get; set; }
+        public string Message { get; set; }
 
         public bool IsSuccess { get; set; }
 
