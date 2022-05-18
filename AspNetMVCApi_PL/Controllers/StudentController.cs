070d419dfe6ce8c7b0a6734ec8cae834c4de3d36
Melike Aslan <91019037+melikeAslan1@users.noreply.github.com>
2022-05-18 12:06:38 +0300

    Asp net MVC Api DI işlemleri için Unity.WebApi paketi kuruldu. UnityConfig içine ayarlar eklendi. Global.asax içine UnityConfig ayarı eklendi. Student api GetAllStudents başarılı test edildi. Student apiye HttpPost metodu yazıldı. Postman ile test edildi.

--- AspNetMVCApi_PL/Controllers/StudentController.cs
+++ AspNetMVCApi_PL/Controllers/StudentController.cs
@@ -1,14 +1,12 @@
 ﻿using AspNetMVCApi_BLL.Contracts;
 using AspNetMVCApi_EL.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
+
 
 namespace AspNetMVCApi_PL.Controllers
 {
+    [System.Web.Http.RoutePrefix("s")]
     public class StudentController : ApiController
     {
         private readonly IStudentService _studentService;
@@ -18,13 +16,14 @@ namespace AspNetMVCApi_PL.Controllers
             _studentService = studentService;
         }
 
-
-        // GET api/<controller>
+        // GET s --> prefix var böyle çağrılır
+        // GET api/student/GetAllStudents
+        [System.Web.Http.Route("")]
         public ResponseData GetAllStudents()
         {
             try
             {
-                var result = _studentService.GetAllStudent();
+                var result = _studentService.GetAllStudent().Data;
                 return new ResponseData() { IsSuccess = true, Data = result };
             }
             catch (Exception ex)
@@ -41,11 +40,48 @@ namespace AspNetMVCApi_PL.Controllers
 
         // GET api/<controller>/5
 
+        //öğrenci ekleme
+        [HttpPost]
+        [System.Web.Http.Route("")]
+
+        public ResponseData AddStudent([FromBody] StudentVM model)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return new ResponseData()
+                    {
+                        IsSuccess = false,
+                        Message = "Veri girişleri düzgün olmalıdır!"
+                    };
+                }
+                model.RegisterDate = DateTime.Now;
+                ResponseData result = _studentService.AddStudent(model);
+                return new ResponseData()
+                {
+                    IsSuccess = true,
+                    Message = "Yeni öğrenci kaydı yapılmıştır."
+                };
+            }
+            catch (Exception ex)
+            {
+                // ex loglanabilir
+                return new ResponseData()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+
+
+
         // POST api/<controller>
 
         // PUT api/<controller>/5
 
         // DELETE api/<controller>/5
     }
-
 }
